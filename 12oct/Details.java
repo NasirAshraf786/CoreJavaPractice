37b2491b6d31f5e7b4629e17818d5b0940c9a806
NASIR ASHRAF <90701206+NasirAshraf786@users.noreply.github.com>
2022-10-19 15:17:38 +0530

    Add files via upload

---
+++ 12oct/Details.java
@@ -0,0 +1,11 @@
+ /*PROGRAM TO PRINT DETAILS USING COMMAND LINE ARGUMENT
+ 
+ class Details
+{
+	public static void main(String args[])
+	{
+		
+		System.out.println("NAME : " +args[0]+ " " +args[1]+ "\nCOURSE : " +args[2]+ "\nAGE : " +args[3]+ "\nGRADE : " +args[4]);
+		
+	}
+}
